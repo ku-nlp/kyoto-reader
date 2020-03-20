0038204f9580503d77739664ebf1f907b36e9319
nobu-g <ueda-n127@hotmail.co.jp>
2020-03-20 18:39:18 +0900

    move test files

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ data_dir = Path(__file__).parent / 'data'
 
 @pytest.fixture()
 def fixture_kyoto_reader():
-    reader = KyotoReader(data_dir,
+    reader = KyotoReader(data_dir / 'knp',
                          target_cases=ALL_CASES,
                          target_corefs=ALL_COREFS)
     yield reader
