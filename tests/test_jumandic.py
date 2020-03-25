20fa260e8ac8a57400599fedbf2c28f5fb8ada79
nobu-g <ueda-n127@hotmail.co.jp>
2020-03-25 15:23:49 +0900

    fix test

--- tests/test_jumandic.py
+++ tests/test_jumandic.py
@@ -10,7 +10,7 @@ data_dir = Path(__file__).parent / 'data' / 'jumandic'
 with data_dir.joinpath('test.dic').open() as fin:
     objs = parse(fin.read())
 
-test_cases = [(obj, json.load(path.open())) for obj, path in zip(objs, data_dir.glob('*.json'))]
+test_cases = [(obj, json.load(path.open())) for obj, path in zip(objs, sorted(data_dir.glob('*.json')))]
 
 
 @pytest.mark.parametrize('test_case', test_cases)
