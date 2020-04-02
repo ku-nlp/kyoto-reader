4aa51915deb3966e5ae7abddd01c6b3978fec24e
nobu-g <ueda-n127@hotmail.co.jp>
2020-04-02 12:23:44 +0900

    fix

--- src/kyoto_reader/scripts/split_corpus.py
+++ src/kyoto_reader/scripts/split_corpus.py
@@ -1,14 +1,15 @@
 import re
-import sys
 import argparse
 from typing import Dict
 from pathlib import Path
 
 
-# cat 9798673.knp | python split_corpus.py --output-dir knp
+# python split_corpus.py --input-dir /somewhere/kc/input --output-dir /somewhere/kc/output
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--input-dir', '-o', default=None, type=str,
+                        help='path to input knp directory')
     parser.add_argument('--output-dir', '-o', default=None, type=str,
                         help='path to output knp directory')
     args = parser.parse_args()
@@ -17,18 +18,20 @@ def main():
 
     docs: Dict[str, str] = {}
     did = None
-    buff = ''
-    for line in sys.stdin:
-        if line.startswith('# S-ID'):
-            match = sid_pat.match(line.strip())
-            # sid = match.group(1) + '-' + match.group(2)
-            if did != match.group(1):
-                if did is not None:
-                    docs[did] = buff
-                    buff = ''
-                did = match.group(1)
-        buff += line
-    docs[did] = buff
+    for path in Path(args.input_dir).glob('**/*.knp'):
+        with path.open() as f:
+            buff = ''
+            for line in f:
+                if line.startswith('# S-ID'):
+                    match = sid_pat.match(line.strip())
+                    # sid = match.group(1) + '-' + match.group(2)
+                    if did != match.group(1):
+                        if did is not None:
+                            docs[did] = buff
+                            buff = ''
+                        did = match.group(1)
+                buff += line
+            docs[did] = buff
 
     output_dir = Path(args.output_dir)
     for did, knp_string in docs.items():
