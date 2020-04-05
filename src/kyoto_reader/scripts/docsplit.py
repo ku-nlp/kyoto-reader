dfae7a3318bc9a78cee5d5306200c77da090d452
nobu-g <ueda-n127@hotmail.co.jp>
2020-04-05 16:22:35 +0900

    fix

--- src/kyoto_reader/scripts/docsplit.py
+++ src/kyoto_reader/scripts/docsplit.py
@@ -3,6 +3,27 @@ import argparse
 from typing import Dict
 from pathlib import Path
 
+SID_PTN = re.compile(r'^# S-ID:((\d{9})|(w\d{6}-\d{10}))-([0-9-]+) .*$')
+
+
+def read(path: Path) -> Dict[str, str]:
+    docs: Dict[str, str] = {}
+    did = None
+    with path.open() as f:
+        buff = ''
+        for line in f:
+            if line.startswith('# S-ID'):
+                match = SID_PTN.match(line.strip())
+                # sid = match.group(1) + '-' + match.group(4)
+                if did != match.group(1):
+                    if did is not None:
+                        docs[did] = buff
+                        buff = ''
+                    did = match.group(1)
+            buff += line
+        docs[did] = buff
+    return docs
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -12,28 +33,13 @@ def main():
                         help='path to output knp directory')
     args = parser.parse_args()
 
-    sid_pat = re.compile(r'^# S-ID:((\d{9})|(w\d{6}-\d{10}))-([0-9-]+) .*$')
-
     docs: Dict[str, str] = {}
-    did = None
     for path in Path(args.input_dir).glob('**/*.knp'):
-        with path.open() as f:
-            buff = ''
-            for line in f:
-                if line.startswith('# S-ID'):
-                    match = sid_pat.match(line.strip())
-                    # sid = match.group(1) + '-' + match.group(4)
-                    if did != match.group(1):
-                        if did is not None:
-                            docs[did] = buff
-                            buff = ''
-                        did = match.group(1)
-                buff += line
-            docs[did] = buff
+        docs.update(read(path))
 
-    output_dir = Path(args.output_dir)
     for did, knp_string in docs.items():
-        with output_dir.joinpath(f'{did}.knp').open(mode='w') as f:
+        out_path = Path(args.output_dir) / f'{did}.knp'
+        with out_path.open(mode='w') as f:
             f.write(knp_string)
 
 
