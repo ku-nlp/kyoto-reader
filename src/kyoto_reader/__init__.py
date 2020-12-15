88568a20c1a2f9898b85b7e986a4f4b3d5d23608
nobu-g <ueda-n127@hotmail.co.jp>
2020-12-15 22:58:24 +0900

    move Document class to another file

--- src/kyoto_reader/__init__.py
+++ src/kyoto_reader/__init__.py
@@ -2,7 +2,9 @@ from pathlib import Path
 __version__ = Path(__file__).parent.joinpath('VERSION').open().read().rstrip()
 
 
-from .reader import KyotoReader, Document, Sentence
+from .reader import KyotoReader
+from .document import Document
+from .sentence import Sentence
 from .base_phrase import BasePhrase
 from .pas import Predicate, BaseArgument, Argument, SpecialArgument, Pas
 from .coreference import Mention, Entity
