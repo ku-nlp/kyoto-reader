23d57dfb88e036ca921c94d87106ca478e54ee31
nobu-g <ueda-n127@hotmail.co.jp>
2021-03-22 01:02:47 +0900

    fix preprocess option

--- src/kyoto_reader/scripts/template.mk
+++ src/kyoto_reader/scripts/template.mk
@@ -32,8 +32,10 @@ $(ORIG_KNP_DONE): $(CORPUS_KNPS)
 add-feats: $(OUT_KNPS)
 
 # knp -dpnd -read-feature
+# -dpnd-fast: 格解析を行わない
+# -read-feature: <rel>タグを消さない
 $(OUT_KNPS): $(OUT_KNP_DIR)/%.knp: $(ADD_SEMS_KNP_DIR)/%.knp
-	mkdir -p $(dir $@) && cat $< | $(KNP) -tab -dpnd -read-feature > $@ || rm -f $@
+	mkdir -p $(dir $@) && cat $< | $(KNP) -tab -dpnd-fast -read-feature > $@ || rm -f $@
 
 # add_sems.py
 $(ADD_SEMS_KNPS): $(ADD_SEMS_KNP_DIR)/%.knp: $(ORIG_KNP_DIR)/%.knp
