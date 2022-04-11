41a3be25cfc6a2eb5e9c7157c60eb2fde6227803
nobu-g <ueda-n127@hotmail.co.jp>
2022-04-11 14:44:04 +0900

    fix docsplit

--- src/kyoto_reader/scripts/docsplit.py
+++ src/kyoto_reader/scripts/docsplit.py
@@ -1,5 +1,4 @@
 import argparse
-from typing import Dict
 from pathlib import Path
 
 from kyoto_reader import KyotoReader
@@ -13,14 +12,12 @@ def main():
                         help='path to output knp directory')
     args = parser.parse_args()
 
-    docs: Dict[str, str] = {}
-    for path in Path(args.input_dir).glob('**/*.knp'):
-        docs.update(KyotoReader._read_knp_wrapper(path, did_from_sid=True))
+    reader = KyotoReader(args.input_dir, did_from_sid=True)
 
-    for did, knp_string in docs.items():
+    for did in reader.doc_ids:
         out_path = Path(args.output_dir) / f'{did}.knp'
         with out_path.open(mode='w') as f:
-            f.write(knp_string)
+            f.write(reader.get_knp(did))
 
 
 if __name__ == '__main__':
